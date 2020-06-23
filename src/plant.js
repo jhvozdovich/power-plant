909fae700e41536613687edbf8a29d375af0861b
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-23 10:22:25 -0700

    refactor with values and currying

--- src/plant.js
+++ src/plant.js
@@ -1,32 +1,34 @@
-const changePlantState = (plant, prop) => {
-  return {
-    ...plant,
-    [prop]: (plant[prop] || 0) + 1
+
+const changeState = prop => {
+  return value => {
+    return state => ({
+      ...state,
+      [prop]: (state[prop] || 0) + value
+    })
   }
 }
+// const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// };
 
-const hydrate = (plant) => {
-  return {
-    ...plant,
-    water: (plant.water || 0) + 1
-  }
-};
+// const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) + 1
+//   }
+// };
 
-const feed = (plant) => {
-  return {
-    ...plant,
-    soil: (plant.soil || 0) + 1
-  }
-};
-
-const giveLight = (plant) => {
-  return {
-    ...plant,
-    light: (plant.light || 0) + 1
-  }
-};
+// const giveLight = (plant) => {
+//   return {
+//     ...plant,
+//     light: (plant.light || 0) + 1
+//   }
+// };
 
 let plant = { soil: 0, light: 0, water: 0 }
 
 
-let Sansevieria = changePlantState(plant, water);
+let Sansevieria = changeState(plant, power, 0);
