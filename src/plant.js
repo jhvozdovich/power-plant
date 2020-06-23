7498e6ae0f2673860a0c9a77188d84faffef5cdc
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-23 10:04:37 -0700

    refactor to general plant state changes

--- src/plant.js
+++ src/plant.js
@@ -1,21 +1,32 @@
-class Plant {
-  constructor() {
-    this.water = 0;
-    this.soil = 0;
-    this.light = 0;
-  }
-
-  hydrate() {
-    this.water++
-  }
-
-  giveLight() {
-    this.light++
-  }
-
-  feed() {
-    this.soil++
+const changePlantState = (plant, prop) => {
+  return {
+    ...plant,
+    [prop]: (plant[prop] || 0) + 1
   }
 }
 
+const hydrate = (plant) => {
+  return {
+    ...plant,
+    water: (plant.water || 0) + 1
+  }
+};
 
+const feed = (plant) => {
+  return {
+    ...plant,
+    soil: (plant.soil || 0) + 1
+  }
+};
+
+const giveLight = (plant) => {
+  return {
+    ...plant,
+    light: (plant.light || 0) + 1
+  }
+};
+
+let plant = { soil: 0, light: 0, water: 0 }
+
+
+let Sansevieria = changePlantState(plant, water);
