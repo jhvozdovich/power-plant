81d73b8a5704579ba04430fe814862e68665013d
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-23 13:52:16 -0700

    remove console logs

--- src/main.js
+++ src/main.js
@@ -43,11 +43,8 @@ $(document).ready(function () {
   }, 7500);
 
   let htmlAngyBulba = "<img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/162292c0-dd68-48c0-a97a-df1ed2c79da3/dc4piz2-68ba2897-a797-469e-837c-7a030739957a.png/v1/fill/w_700,h_350,strp/angry_bulbasaur_by_lukeblizz_dc4piz2-350t.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvMTYyMjkyYzAtZGQ2OC00OGMwLWE5N2EtZGYxZWQyYzc5ZGEzXC9kYzRwaXoyLTY4YmEyODk3LWE3OTctNDY5ZS04MzdjLTdhMDMwNzM5OTU3YS5wbmciLCJ3aWR0aCI6Ijw9MTgwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.9N7oWfSVUnrcouKwbY7sQOn_d5xS8hrorKMyB1U_1UU' width=80% alt='bulbasaur mood'>";
-
   let htmlHypeBulba = "<img src='https://i.ytimg.com/vi/abe7iRiZkpY/maxresdefault.jpg' width=80% alt='bulbasaur mood'>";
-
   let htmlNormalBulba = "<img src='https://static.pokemonpets.com/images/monsters-images-300-300/1-Bulbasaur.png' width=80% alt='bulbasaur mood'>";
-
 
   const bulbaHappines = () => {
     const newState = stateChanger(changeState("light")(0));
